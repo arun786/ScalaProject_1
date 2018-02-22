7500a176c1de3c75c39af0c64ddfc827ac654dd7
Adwiti789 <adwiti1975@gmail.com>
2018-02-22 02:19:10 -0700

    while loop in Scala and Java

---
+++ src/Loop/WhileLoopInJava.java
@@ -0,0 +1,22 @@
+package Loop;
+
+/**
+ * Created by Adwiti on 2/22/2018.
+ */
+public class WhileLoopInJava {
+
+    public static void main(String[] args) {
+        whileLoop(10);
+    }
+
+    public static void whileLoop(int a) {
+        int index = 0;
+
+        while (index < a) {
+            System.out.println(index);
+            index++;
+        }
+    }
+
+
+}
